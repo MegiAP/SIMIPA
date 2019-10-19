2a186544019fac24470310d898abd9caa4edc287
dhiandraaa <d.adithya.a@gmail.com>
2019-10-19 21:26:40 +0700

    fix profilactivity.java

--- app/src/main/java/com/vincode/simipa/ui/profil/ProfilActivity.java
+++ app/src/main/java/com/vincode/simipa/ui/profil/ProfilActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.vincode.simipa.R;
+import com.vincode.simipa.SharedPrefManager;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    TextView tvProfilName, tvProfilNPM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        tvProfilName = (TextView)findViewById(R.id.tv_name_profil);
+        tvProfilNPM = findViewById(R.id.tv_npm_profil);
+
+        tvProfilName.setText(SharedPrefManager.getInstance(this).getUser().getDisplayName());
+        tvProfilNPM.setText(SharedPrefManager.getInstance(this).getUser().getUserLogin());
 
         ImageView imgBack = findViewById(R.id.img_back_profil);
         imgBack.setOnClickListener(new View.OnClickListener() {
